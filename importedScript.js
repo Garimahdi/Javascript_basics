bff5db6475b5c63349613e0cd155db54ebcad216
GA <g.astaghfari@gmail.com>
2021-12-22 00:00:16 +0700

    Basic js: variable

---
+++ importedScript.js
@@ -0,0 +1,7 @@
+const c = 4184;
+let waterMass, waterTemp;
+waterMass = 20;
+waterTemp = 273;
+heat = waterMass*c*waterTemp
+unit = 'Joule'
+console.log('The heat of 20kg of water with temperature of 273 kelvin = ',heat,unit)
